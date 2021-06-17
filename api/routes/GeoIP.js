540f6eb078c24ef5ee2146bb2eac7d68bf3303c3
Josias <fulsonr30@hotmail.com>
2021-06-17 02:35:22 -0400

    GEOIP UPDATE

--- api/routes/GeoIP.js
+++ api/routes/GeoIP.js
@@ -1,45 +1,32 @@
 const express = require("express");
-const os = require("os");
-const publicIp = require("public-ip");
-var geoip = require("geoip-lite");
-var geo; 
-var ip;
+const { Navigator } = require("node-navigator");
+const navigator = new Navigator();
 var info;
+var lat;
+var lon;
 
 
 const router = express.Router(); 
 
 router.get("/", (req, res) => {
 
+  navigator.geolocation.getCurrentPosition((success, error) => {
+  if (error) {console.error(error)}
+  else {
+    lat = success.latitude;
+    lon = success.longitude;
 
-var interfaces = os.networkInterfaces();
-var addresses = [];
-for (var k in interfaces) {
-  for (var k2 in interfaces[k]) {
-    var address = interfaces[k][k2];
-    if (address.family === "IPv4" && !address.internal) {
-      addresses.push(address.address);
-    }
   }
-}
 
-(async () => {
-  ip = await publicIp.v4();
-  geo = geoip.lookup(ip);
+  info = { "Latitude": lat, "Longitude": lon };
 
- 
-info = {"publicip" : ip,
-    "privateip" : addresses, geo}
+ // console.log(info, "info");
 
-  
-  return res
-    .status(200)
-    .send(JSON.stringify(info));
-})();
-
-
+  return res.status(200).send(JSON.stringify(info));
+});
 
 
 });
+
 
 module.exports = router;
